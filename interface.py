262f3a386a385826d6748a41f5a79a48f28ae440
zabelca <zabelca01@gmail.com>
2023-02-03 14:01:40 -0500

    Added basic interface script

---
+++ interface.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+
+def button1_clicked():
+    print("Button 1 clicked")
+
+def button2_clicked():
+    print("Button 2 clicked")
+
+def button3_clicked():
+    print("Button 3 clicked")
+
+def button4_clicked():
+    print("Button 4 clicked")
+
+def button5_clicked():
+    print("Button 5 clicked")
+
+def button6_clicked():
+    print("Button 6 clicked")
+
+root = tk.Tk()
+root.title("6 Buttons Interface")
+root.geometry("200x250")
+
+button1 = tk.Button(root, text="Button 1", command=button1_clicked)
+button1.pack()
+
+button2 = tk.Button(root, text="Button 2", command=button2_clicked)
+button2.pack()
+
+button3 = tk.Button(root, text="Button 3", command=button3_clicked)
+button3.pack()
+
+button4 = tk.Button(root, text="Button 4", command=button4_clicked)
+button4.pack()
+
+button5 = tk.Button(root, text="Button 5", command=button5_clicked)
+button5.pack()
+
+button6 = tk.Button(root, text="Button 6", command=button6_clicked)
+button6.pack()
+
+root.mainloop()
+                                                
